8a29f049003c5e9c24b843452943628cac5365f2
Igor Chvalun <igor.chvalun@investengine.com>
2023-09-28 10:01:06 +0530

    Done

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,28 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            int width = (cathetusLength - 1) * 2 + 1;
+            String line = "";
+            for (int j = 0; j <= width; j++) {
+                if(j < cathetusLength) {
+                    int d = cathetusLength - j;
+                    if(d <= i) {
+                        line = line + "*";
+                    } else {
+                        line = line + " ";
+                    }
+                } else {
+                    int c = j - cathetusLength + 1;
+                    if(c <= i) {
+                        //line = line + c;
+                    } else {
+                        //line = line + "";
+                    }
+                }
+            }
+            System.out.println(line);
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,28 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            int width = (cathetusLength - 1) * 2 + 1;
+            String line = "";
+            for (int j = 1; j <= width; j++) {
+                if(j < cathetusLength) {
+                    int d = cathetusLength - j + 1;
+                    if(d <= i) {
+                        line = line + d;
+                    } else {
+                        line = line + " ";
+                    }
+                } else {
+                    int c = j - cathetusLength + 1;
+                    if(c <= i) {
+                        line = line + c;
+                    } else {
+                        line = line + "";
+                    }
+                }
+            }
+            System.out.println(line);
+        }
     }
 
     public static void main(String[] args) {
